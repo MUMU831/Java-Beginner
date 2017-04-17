0e4acee21441b0d6701e93b9f282484e8c892011
MUMU831 <liumuyinju@hotmail.com>
2017-04-18 00:38:57 +0800

    # WARNING: head commit changed in the meantime
    FirstJavaProject as Homework

--- everysingleday/src/everysingleday/everyminutecounts.java
+++ everysingleday/src/everysingleday/everyminutecounts.java
@@ -5,6 +5,8 @@ public class everyminutecounts {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("null");
+		System.out.println("nul");
+
 	}
 
 }
